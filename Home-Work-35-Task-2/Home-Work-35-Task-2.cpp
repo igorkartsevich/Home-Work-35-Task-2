3cb9c5df509c699f770365806b66a4477d4cf023
igor_kartsevich <71553499+igorkartsevich@users.noreply.github.com>
2022-03-23 20:37:09 +0300

    23/03/01

--- Home-Work-35-Task-2/Home-Work-35-Task-2.cpp
+++ Home-Work-35-Task-2/Home-Work-35-Task-2.cpp
@@ -8,8 +8,7 @@ int main()
     std::vector<int> data = { 1,2,3,4,3,2,3,4,5,4,3,4 };
     
     auto make_ptr = [](std::vector<int> inData) {
-        std::unordered_multiset<int> set;
-        for (auto element : inData) set.emplace(element);
+        std::unordered_multiset<int> set (inData.begin(), inData.end());
         std::cout << set.count(3);
         std::vector<int> vec_unique;
         std::unique_copy(set.begin(), set.end(), back_inserter(vec_unique));
